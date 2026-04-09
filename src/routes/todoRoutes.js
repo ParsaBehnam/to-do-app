fb68f60cdabd5c5c8c511744e40f048c54e61570
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-09 19:03:45 +0300

    configure routes and connecting them to server.js

---
+++ src/routes/todoRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import db from '../db.js'
+
+const router = express.Router()
+
+// Get todos for logged-in user
+router.get('/', (req, res) => {
+
+})
+
+//Create a new todo
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:id' , (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+    
+})
+
+export default router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import path, {dirname} from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -22,5 +24,9 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
+
+//Routes
+app.use('/auth', authRoutes)
+app.use('/todos', todoRoutes)
 
 app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`))
