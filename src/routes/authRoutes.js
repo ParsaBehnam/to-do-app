f35385e7a9e4c4d801efe323db7d9018644499c6
ParsaBehnam <183300741+ParsaBehnam@users.noreply.github.com>
2026-04-09 22:11:47 +0300

    add /auth/register post method and connect it to the database using bcrypt hashpassword function and sign a jwt token to it

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,43 @@ import db from '../db.js'
 const router = express.Router()
 
 router.post('/register', (req, res) => {
+    const {username, password} = req.body
+    // save the username and an irreversible encrypted password to the database
+    
+    //encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8)
 
-})
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
+
+        // add the user's first todo 
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task)
+             VALUES (?, ?)`)
+             insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET,
+            {expiresIn: '24h'})
+
+        res.json({token})
+    }
+        
+      catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+
+    
+      })
 
 router.post('/login', (req, res) => {
-    
+    // we get their email and we lookup the password associated with that
+    // email in the database
+    // we compare it with the entered password using the same encryption key
 })
+
+
 
 export default router
